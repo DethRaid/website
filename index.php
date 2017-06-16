ae264f606fd66811878c98553701628dbbf4b92a
David Dubois <dexcelstraun7@gmail.com>
2017-06-16 15:32:27 -0400

    typo

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
                 </div>
                 </div>
                 <div class="item">
-                <img class="second-slide" src="ing/nova_code.png" alt="Second slide">
+                <img class="second-slide" src="img/nova_code.png" alt="Second slide">
                 <div class="container">
                     <div class="carousel-caption">
                     <h1>Nova Renderer</h1>
