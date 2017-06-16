19c9dbf8c3e15ac4e0d6898470a6be013e5c00c5
David Dubois <dexcelstraun7@gmail.com>
2017-06-16 15:22:30 -0400

    Fixed a big probs

--- index.php
+++ index.php
@@ -15,58 +15,58 @@
     <body>
         <?php require_once "Header.php" ?>
 
-        <div class="container">
         <!-- Carousel
-            ================================================== -->
-            <div id="myCarousel" class="carousel slide" data-ride="carousel">
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
-                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                <li data-target="#myCarousel" data-slide-to="1"></li>
-                <li data-target="#myCarousel" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner" role="listbox">
-                <div class="item active">
-                <img class="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
-                <div class="container">
-                    <div class="carousel-caption">
-                    <h1>DRAGON Shaderpack</h1>
-                    <p>The first shaderpack to implement physically-based rendering. DRAGON continues to push the envelope of what's possible with Minecraft: Java Edition shaders</p>
-                    <p><a class="btn btn-lg btn-primary" href="dragon/index.php" role="button">Learn more</a></p>
-                    </div>
-                </div>
-                </div>
-                <div class="item">
-                <img class="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
-                <div class="container">
-                    <div class="carousel-caption">
-                    <h1>Nova Renderer</h1>
-                    <p>Thing Minecraft: Java Edition's renderer is too slow? The Nova Renderer replaces the vanilla renderer with OpenGL 4.5, C++14, and a devotion to usability.</p>
-                    <p><a class="btn btn-lg btn-primary" href="https://github.com/NovaMods/nova-renderer" role="button">Github Repo</a></p>
-                    </div>
-                </div>
-                </div>
-                <div class="item">
-                <img class="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
-                <div class="container">
-                    <div class="carousel-caption">
-                    <h1>Automatic Machine Translation</h1>
-                    <p>Imagine a program that can take two large bodies of text in two unrelated languages and figure out how to translate between them. We're making that program.</p>
-                    <p><a class="btn btn-lg btn-primary" href="https://github.com/DethRaid/voynich-translation" role="button">Github Repo</a></p>
-                    </div>
-                </div>
+        ================================================== -->
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+            <img class="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
+            <div class="container">
+                <div class="carousel-caption">
+                <h1>DRAGON Shaderpack</h1>
+                <p>The first shaderpack to implement physically-based rendering. DRAGON continues to push the envelope of what's possible with Minecraft: Java Edition shaders</p>
+                <p><a class="btn btn-lg btn-primary" href="dragon/index.php" role="button">Learn more</a></p>
                 </div>
             </div>
-            <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-            </div><!-- /.carousel -->
+            </div>
+            <div class="item">
+            <img class="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
+            <div class="container">
+                <div class="carousel-caption">
+                <h1>Nova Renderer</h1>
+                <p>Thing Minecraft: Java Edition's renderer is too slow? The Nova Renderer replaces the vanilla renderer with OpenGL 4.5, C++14, and a devotion to usability.</p>
+                <p><a class="btn btn-lg btn-primary" href="https://github.com/NovaMods/nova-renderer" role="button">Github Repo</a></p>
+                </div>
+            </div>
+            </div>
+            <div class="item">
+            <img class="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
+            <div class="container">
+                <div class="carousel-caption">
+                <h1>Automatic Machine Translation</h1>
+                <p>Imagine a program that can take two large bodies of text in two unrelated languages and figure out how to translate between them. We're making that program.</p>
+                <p><a class="btn btn-lg btn-primary" href="https://github.com/DethRaid/voynich-translation" role="button">Github Repo</a></p>
+                </div>
+            </div>
+            </div>
+        </div>
+        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+        </div><!-- /.carousel -->
 
+        <div class="container">
             <h1>Main projects</h1>
 
             <div class="row">
