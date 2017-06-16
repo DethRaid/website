e30880002d876c55da5001144aa13f2d25a526a4
David Dubois <dexcelstraun7@gmail.com>
2017-06-16 15:21:03 -0400

    Revised frontpage

--- index.php
+++ index.php
@@ -16,10 +16,58 @@
         <?php require_once "Header.php" ?>
 
         <div class="container">
-            <div class="jumbotron">
-                <p>Hello, and welcome to the homepage of David Dubois!</p>
-                <p>Projects include the following:</p>
+        <!-- Carousel
+            ================================================== -->
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                <img class="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
+                <div class="container">
+                    <div class="carousel-caption">
+                    <h1>DRAGON Shaderpack</h1>
+                    <p>The first shaderpack to implement physically-based rendering. DRAGON continues to push the envelope of what's possible with Minecraft: Java Edition shaders</p>
+                    <p><a class="btn btn-lg btn-primary" href="dragon/index.php" role="button">Learn more</a></p>
+                    </div>
+                </div>
+                </div>
+                <div class="item">
+                <img class="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
+                <div class="container">
+                    <div class="carousel-caption">
+                    <h1>Nova Renderer</h1>
+                    <p>Thing Minecraft: Java Edition's renderer is too slow? The Nova Renderer replaces the vanilla renderer with OpenGL 4.5, C++14, and a devotion to usability.</p>
+                    <p><a class="btn btn-lg btn-primary" href="https://github.com/NovaMods/nova-renderer" role="button">Github Repo</a></p>
+                    </div>
+                </div>
+                </div>
+                <div class="item">
+                <img class="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
+                <div class="container">
+                    <div class="carousel-caption">
+                    <h1>Automatic Machine Translation</h1>
+                    <p>Imagine a program that can take two large bodies of text in two unrelated languages and figure out how to translate between them. We're making that program.</p>
+                    <p><a class="btn btn-lg btn-primary" href="https://github.com/DethRaid/voynich-translation" role="button">Github Repo</a></p>
+                    </div>
+                </div>
+                </div>
             </div>
+            <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+            </div><!-- /.carousel -->
+
+            <h1>Main projects</h1>
 
             <div class="row">
                 <div class="col-md-4">
@@ -32,36 +80,38 @@
 
                 <div class="col-md-4">
                     <div class="content">
-                        <h2>Voynich Manuscript</h2>
-                        <p>In 1912, a Polish book dealer named Wilfred Voynich purchased a mysterious book, which has since been dubbed the "Voynich Manuscript". It was written in an unknown script, with colorful pictures of plants and animals decorating its pages. For over a hundred years no one has been able to decipher this strange text, although many have tried. Now, with the help of deep learning algorithms, I'm making a feeble attempt to gleam some meaning from the manuscript's alien writing.</p>
-                    </div>
-                    <p><a class="btn btn-default" href="voynich/index.php" role="button">Learn Mode &raquo;</a></p>
-                </div>
-
-                <div class="col-md-4">
-                    <div class="content">
                         <h2>DRAGON</h2>
                         <p>DRAGON is a collection of GLSL shaders aimed at the Minecraft GLSL Shaders Mode. The goal of DRAGON is to provide a physically-based lighting model for Minecraft, mostly because I enjoy exploring different lighting models. DRAGON currently features accurate reflections on all materials, varying-softness shadows, and filmic post-processing, and normalmapping.</p>
                     </div>
                     <p><a class="btn btn-default" href="dragon/index.php" role="button">Learn More &raquo;</a></p>
                 </div>
-            </div>
 
-            <div class="row">
                 <div class="col-md-4">
                     <div class="content">
-                        <h2>Jotun Engine</h2>
-                        <p>The Jotun Engine is an OpenGL-based game engine written in C++. It began as an experiment in graphics programming and has grown to be so much more. Incorporating the Open Asset Import Library, GLSL version 130, and a dedication to quality, the Jotun Engine is far more powerful than I could possibly imagine.</p>
-                        </div>
-                    <p><a class="btn btn-default" href="JotunEngine.php" role="button">Learn More &raquo;</a></p>
+                        <h2>Voynich Manuscript</h2>
+                        <p>In 1912, a Polish book dealer named Wilfred Voynich purchased a mysterious book, which has since been dubbed the "Voynich Manuscript". It was written in an unknown script, with colorful pictures of plants and animals decorating its pages. For over a hundred years no one has been able to decipher this strange text, although many have tried. Now, with the help of deep learning algorithms, I'm making a feeble attempt to gleam some meaning from the manuscript's alien writing.</p>
+                    </div>
+                    <p><a class="btn btn-default" href="https://github.com/DethRaid/voynich-translation" role="button">Learn Mode &raquo;</a></p>
                 </div>
+            </div>
 
+            <h1>Secondary projects</h1>
+
+            <div class="row">
                 <div class="col-md-4">
                     <div class="content">
                         <h2>Warfare 2525</h2>
                         <p>Warfare 2525 is a real-time strategy game dedicated to bringing the player's emotions into the gameplay. It began as a simple RTS but quickly grew into an experiment in storytelling. Although still in early development, Warfare 2525 demonstrates many of its core functinalities. Active development is currently on hold due to college. </p>
                     </div>
                     <p><a class="btn btn-default" href="warfare/index.php" role="button">Learn More &raquo;</a></p>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="content">
+                        <h2>Jotun Engine</h2>
+                        <p>The Jotun Engine is an OpenGL-based game engine written in C++. It began as an experiment in graphics programming and has grown to be so much more. Incorporating the Open Asset Import Library, GLSL version 130, and a dedication to quality, the Jotun Engine is far more powerful than I could possibly imagine.</p>
+                        </div>
+                    <p><a class="btn btn-default" href="JotunEngine.php" role="button">Learn More &raquo;</a></p>
                 </div>
 
                 <div class="col-md-4">
